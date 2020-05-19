b6a2fcb3d3bdb9e5b89ac2fd062a5fa1f3addd7b
Malu Schymanski <maluschymanski@Malus-MBP.fritz.box>
2020-05-19 23:15:19 +0200

    state_19-05-2020

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => (
+    <div>
+        <h2>About Page</h2>
+        <p>
+            blablablabla
+        </p>
+        <p>
+            blablabla bli blub
+        </p>
+    </div>
+)
---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Contact = () => (
+    <div>
+        <h2>Contact Page</h2>
+        <p>
+            blablablabla
+        </p>
+        <p>
+            blablabla bli blub
+        </p>
+    </div>
+)
